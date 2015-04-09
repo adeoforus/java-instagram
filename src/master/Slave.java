c34c1e070680109961a6a259cd343b76c6477d7c
Arthur Skobnikoff <arthur.skobnikov@gmail.com>
2015-04-09 22:35:28 +0200

    Cleaned some shit code. But still it remains a lot

---
+++ src/master/Slave.java
@@ -0,0 +1,61 @@
+package master;
+
+import java.io.File;
+
+import master.Task;
+
+
+public class Slave extends BasicSlave{
+	// ATTRIBUTES
+	protected String status = "free";
+
+	
+	protected Task currentTask;
+	
+	
+	// CONSTRUCTOR
+	public Slave(int id, String host, int port) {
+		super(id, host, port);
+	}
+
+
+
+	
+	// GETTERS, SETTERS
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Task getCurrentTask() {
+		return currentTask;
+	}
+
+	public void setCurrentTask(Task currentTask) {
+		this.currentTask = currentTask;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return String
+				.format("BasicSlave [id=%s, host=%s, port=%s, status=%s, \ncurrentTask=%s, \ndbDir=%s, \nrawDataDir=%s]",
+						id, host, port, status,
+						currentTask, dbDir, rawDataDir);
+	}
+
+	
+	// TO STRING
+
+	
+	
+
+		
+	
+	
+}
